5816fc7edd28e47a7d04834b583d755f2a2748cd
martin jakubik <martin.jakubik@sap.com>
2023-03-18 10:44:06 +0100

    added args to format script

--- format-boursorama-csv.sh
+++ format-boursorama-csv.sh
@@ -1,9 +1,37 @@
 #!/bin/bash
 
-WORK_FILE=~/Downloads/export-operations-test-0.csv
+# sets up usage
+USAGE="usage: $0 -i infile --outfile outfile -o outfile"
 
-cp ~/Downloads/export-operations-test.csv $WORK_FILE
+# set up defaults
+infile=~/infile.csv
+outfile=~/outfile.csv
 
-sed -e '1,$s/,/./g' -i '.bak' $WORK_FILE
-sed -e '1,$s/;/,/g' -i '.bak' $WORK_FILE
-sed -e '1,$s/\([0-9]\) \([0-9]\)/\1\2/g' -i '.bak' $WORK_FILE
+# parses and reads command line arguments
+while [ $# -gt 0 ]
+do
+  case "$1" in
+    (-i) infile="$2"; shift;;
+    (--outfile) outfile="$2"; shift;;
+    (-o) outfile="$2"; shift;;
+    (-*) echo >&2 ${USAGE}
+    exit 1;;
+  esac
+  shift
+done
+
+if [[ ! -e  $outfile ]] ; then
+  outfilebasedir=$(basedir $outfile)
+  mkdir -p $outfilebasedir
+fi
+
+echo you entered values
+echo   From infile : $infile
+echo   To          : $outfile
+
+cp $infile $outfile
+
+# formats the boursorama csv in us_EN
+sed -e '1,$s/,/./g' -i '.bak' $outfile
+sed -e '1,$s/;/,/g' -i '.bak' $outfile
+sed -e '1,$s/\([0-9]\) \([0-9]\)/\1\2/g' -i '.bak' $outfile
