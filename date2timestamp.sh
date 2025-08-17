bf7fd2ea4485b6e6b43d2f76a595253caaed6704
martin jakubik <martin.jakubik@sap.com>
2025-08-17 19:16:02 +0200

    corrected date conversion

--- date2timestamp.sh
+++ date2timestamp.sh
@@ -1,14 +1,17 @@
 #!/bin/sh
 
-# Expects a date string like "Wed 05 Jul 2023 04:55:30 PM +03" as first parameter.
-# Evaluate if it's AM or PM
-# remove the "AM/PM" string
-# and set the time correction for the missing "PM" if necessary.
+# expects a date string like "Jan 1, 1950, 6:04:00 PM UTC" as first parameter.
+# evaluates if it's AM or PM
+# removes the "AM/PM" string
+# and sets the time correction for the missing "PM" if necessary.
 
-if [ "${1%?PM}"  !=  "${1}" ]; then
-    timestamp_format=$(( $(date -j -f "%b %e, %Y, %l:%M:%S" "${1%?PM}" "+%s") + 43200 ))
+echo stripping PM: "${1%?PM UTC}"
+if [ "${1%?PM UTC}" != "${1}" ]; then
+    echo it is PM
+    timestamp_format=$(( $(date -j -f "%b %e, %Y, %l:%M:%S" "${1%?PM UTC}" "+%s") + 43200 ))
 else
-    timestamp_format=$(date -j -f "%b %e, %Y, %l:%M:%S" "${1%?AM}" "+%s")
+    echo it is AM
+    timestamp_format=$(date -j -f "%b %e, %Y, %l:%M:%S" "${1%?AM UTC}" "+%s")
 fi
 
 date -r $timestamp_format +%Y-%m-%dT%H:%M:%S
