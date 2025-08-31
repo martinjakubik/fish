bdffc92cfc50177bb5184123e38961697c7f2c02
martin jakubik <martin.jakubik@gmail.com>
2025-08-31 13:57:42 +0200

    echoed all commands for matching photos to a file

--- list-files-with-matching-metadata.sh
+++ list-files-with-matching-metadata.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # sets up usage
-USAGE="usage: $0 -i --inputDir inputDir -d --debug"
+USAGE="usage: $0 -i --inputDir inputDir -c --commandFile -d --debug"
 
 if ! command -v jq >/dev/null 2>&1 ; then
     echo "jq could not be found"
@@ -9,7 +9,8 @@ fi
 
 # set up defaults
 DEBUG=0
-inputDir=~/inputDir
+inputDir=$HOME/inputDir
+command_file=$HOME/gphotoexifupdater.sh
 
 # parses and reads command line arguments
 while [ $# -gt 0 ]
@@ -17,6 +18,8 @@ do
   case "$1" in
     (-i) inputDir="$2"; shift;;
     (--inputDir) inputDir="$2"; shift;;
+    (-c) inputDir="$2"; shift;;
+    (--commandFile) command_file="$2"; shift;;
     (-d) DEBUG=1;;
     (--debug) DEBUG=1;;
     (-*) echo >&2 ${USAGE}
@@ -116,6 +119,7 @@ export -f months_between_dates
 apply_extracted_date_to_photo_or_movie_file() {
     file_name="$1"
     file_create_date_as_epoch="$2"
+    command_file="$3"
     file_create_date_as_exif=$(convert_epoch_date_to_exif_date "$file_create_date_as_epoch")
     photo_create_date_as_exif_tuple=$(exiftool -CreateDate "$file_name")
     photo_create_date_as_exif=${photo_create_date_as_exif_tuple#Create Date*: }
@@ -124,16 +128,23 @@ apply_extracted_date_to_photo_or_movie_file() {
     echo >&2 file create date $file_create_date_as_exif
     echo >&2 photo create date $photo_create_date_as_exif
     echo >&2 months between $months_between
+    echo >&2 command file $command_file
+    if [[ $months_between > 12 ]] ; then
+        echo applying file creation date to photo "$file_name" >> "$HOME/gphotoexifupdater.sh"
+    else
+        echo not applying file creation date to photo "$file_name" >> "$HOME/gphotoexifupdater.sh"
+    fi
 }
 
 export -f apply_extracted_date_to_photo_or_movie_file
 
-update_photo_file_modified_date_with_extracted_meta_data() {
+update_jpeg_file_modified_date_with_extracted_meta_data() {
     file_name="$1"
+    command_file="$2"
     meta_data_file=$(get_photo_meta_data_file "$file_name")
     extracted_creation_date=$(extract_creation_date_from_meta_data_file "$meta_data_file")
     if [ $extracted_creation_date ] ; then
-        apply_extracted_date_to_photo_or_movie_file "$file_name" $extracted_creation_date
+        apply_extracted_date_to_photo_or_movie_file "$file_name" $extracted_creation_date "$command_file"
     else
         echo
         return 1
@@ -141,18 +152,16 @@ update_photo_file_modified_date_with_extracted_meta_data() {
     echo
 }
 
-export -f update_photo_file_modified_date_with_extracted_meta_data
+export -f update_jpeg_file_modified_date_with_extracted_meta_data
 
-update_movie_file_modified_date_with_extracted_meta_data() {
+update_mp4_file_modified_date_with_extracted_meta_data() {
     file_name="$1"
     meta_data_file=$(get_photo_meta_data_file "$file_name")
     echo meta data file: "${meta_data_file}"
     echo
 }
 
-export -f update_movie_file_modified_date_with_extracted_meta_data
+export -f update_mp4_file_modified_date_with_extracted_meta_data
 
-find "$inputDir" -type f -iname '*.jpg' -ok sh -c 'update_photo_file_modified_date_with_extracted_meta_data "{}"' \;
-find "$inputDir" -type f -iname '*.png' -ok sh -c 'update_photo_file_modified_date_with_extracted_meta_data "{}"' \;
-find "$inputDir" -type f -iname '*.mp4' -ok sh -c 'update_movie_file_modified_date_with_extracted_meta_data "{}"' \;
-find "$inputDir" -type f -iname '*.mov' -ok sh -c 'update_movie_file_modified_date_with_extracted_meta_data "{}"' \;
+echo > "$command_file"
+find "$inputDir" -type f -iname '*.jpg' -exec sh -c 'update_jpeg_file_modified_date_with_extracted_meta_data "{}" "$command_file"' \;
