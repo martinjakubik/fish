94a83ca2d92e622ce7153ce5d8bf5bee81dc76a4
martin jakubik <martin.jakubik@sap.com>
2025-09-07 09:53:13 +0200

    added debug flag

--- get_gphoto_meta_data_file.sh
+++ get_gphoto_meta_data_file.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+DEBUG=1
 MAX_LENGTH=52
 photo_file="$1"
 photo_file_base_name=$(basename "$photo_file")
@@ -10,31 +11,30 @@ length_of_suffix=${#shortened_suffix}
 json_extension=".json"
 length_of_json_extension=${#json_extension}
 total_length=$(( length_of_file_name+length_of_suffix+length_of_json_extension ))
-echo >&2 length of file name $length_of_file_name
-echo >&2 length of suffix $length_of_suffix
-echo >&2 total length $total_length max length $MAX_LENGTH
+if [[ $DEBUG = 1 ]] ; then echo >&2 length of file name $length_of_file_name; fi
+if [[ $DEBUG = 1 ]] ; then echo >&2 length of suffix $length_of_suffix; fi
+if [[ $DEBUG = 1 ]] ; then echo >&2 total length $total_length max length $MAX_LENGTH; fi
 if [[ ${photo_file_base_name%[ ]-[ ]*[0-9].jpg} != ${photo_file_base_name} ]] ; then
-    echo >&2 matched++++++++++++++++++
     photo_file_without_jpg=${photo_file%.jpg}
     photo_file_base_name_without_jpg=${photo_file_base_name%.jpg}
     length_of_file_name_without_jpg=${#photo_file_base_name_without_jpg}
     total_length_without_jpg=$(( length_of_file_name_without_jpg+length_of_suffix+length_of_json_extension ))
-    echo >&2 length of file name without jpg $length_of_file_name_without_jpg
-    echo >&2 length of suffix $length_of_suffix
-    echo >&2 total length $total_length_without_jpg max length $MAX_LENGTH
+    if [[ $DEBUG = 1 ]] ; then echo >&2 length of file name without jpg $length_of_file_name_without_jpg; fi
+    if [[ $DEBUG = 1 ]] ; then echo >&2 length of suffix $length_of_suffix; fi
+    if [[ $DEBUG = 1 ]] ; then echo >&2 total length $total_length_without_jpg max length $MAX_LENGTH; fi
     if [[ $total_length_without_jpg -gt $MAX_LENGTH ]] ; then
         number_of_characters_to_keep=$(( ${#shortened_suffix}-1 ))
-        echo >&2 number of characters to keep: $number_of_characters_to_keep
+        if [[ $DEBUG = 1 ]] ; then echo >&2 number of characters to keep: $number_of_characters_to_keep; fi
         excess_length_without_jpg=$(( total_length_without_jpg-MAX_LENGTH ))
-        echo >&2 excess length without jpg $excess_length
+        if [[ $DEBUG = 1 ]] ; then echo >&2 excess length without jpg $excess_length; fi
         if [[ excess_length_without_jpg -ge 0 ]] ; then
             number_of_characters_to_keep=$(( number_of_characters_to_keep-excess_length_without_jpg ))
         fi
-        echo >&2 number of characters to keep after shortening: $number_of_characters_to_keep
+        if [[ $DEBUG = 1 ]] ; then echo >&2 number of characters to keep after shortening: $number_of_characters_to_keep; fi
         if [[ number_of_characters_to_keep -ge 0 ]] ; then
             shortened_suffix=${suffix_for_json_files::number_of_characters_to_keep}
-            echo >&2 number of characters to keep $number_of_characters_to_keep
-            echo >&2 shortened suffix: \"$shortened_suffix\"
+            if [[ $DEBUG = 1 ]] ; then echo >&2 number of characters to keep $number_of_characters_to_keep; fi
+            if [[ $DEBUG = 1 ]] ; then echo >&2 shortened suffix: \"$shortened_suffix\"; fi
             echo "$photo_file_without_jpg"$shortened_suffix.json
         else
             echo >&2 cannot create metadata filename for: \"$photo_file\"
@@ -45,34 +45,34 @@ if [[ ${photo_file_base_name%[ ]-[ ]*[0-9].jpg} != ${photo_file_base_name} ]] ; 
     fi
 elif [[ $total_length -gt $MAX_LENGTH ]] ; then
     number_of_characters_to_keep=$(( ${#shortened_suffix}-1 ))
-    echo >&2 number of characters to keep: $number_of_characters_to_keep
+    if [[ $DEBUG = 1 ]] ; then echo >&2 number of characters to keep: $number_of_characters_to_keep; fi
     excess_length=$(( total_length-MAX_LENGTH ))
-    echo >&2 excess length $excess_length
+    if [[ $DEBUG = 1 ]] ; then echo >&2 excess length $excess_length; fi
     if [[ excess_length -ge 0 ]] ; then
         number_of_characters_to_keep=$(( number_of_characters_to_keep-excess_length ))
     fi
-    echo >&2 number of characters to keep after shortening: $number_of_characters_to_keep
+    if [[ $DEBUG = 1 ]] ; then echo >&2 number of characters to keep after shortening: $number_of_characters_to_keep; fi
     if [[  number_of_characters_to_keep -lt 0 ]] ; then
         photo_file_without_jpg=${photo_file%.jpg}
         photo_file_base_name_without_jpg=${photo_file_base_name%.jpg}
         length_of_file_name_without_jpg=${#photo_file_base_name_without_jpg}
         total_length_without_jpg=$(( length_of_file_name_without_jpg+length_of_suffix+length_of_json_extension ))
-        echo >&2 length of file name without jpg $length_of_file_name_without_jpg
-        echo >&2 length of suffix $length_of_suffix
-        echo >&2 total length $total_length_without_jpg max length $MAX_LENGTH
+        if [[ $DEBUG = 1 ]] ; then echo >&2 length of file name without jpg $length_of_file_name_without_jpg; fi
+        if [[ $DEBUG = 1 ]] ; then echo >&2 length of suffix $length_of_suffix; fi
+        if [[ $DEBUG = 1 ]] ; then echo >&2 total length $total_length_without_jpg max length $MAX_LENGTH; fi
         if [[ $total_length_without_jpg -gt $MAX_LENGTH ]] ; then
             number_of_characters_to_keep=$(( ${#shortened_suffix}-1 ))
-            echo >&2 number of characters to keep: $number_of_characters_to_keep
+            if [[ $DEBUG = 1 ]] ; then echo >&2 number of characters to keep: $number_of_characters_to_keep; fi
             excess_length_without_jpg=$(( total_length_without_jpg-MAX_LENGTH ))
-            echo >&2 excess length without jpg $excess_length
+            if [[ $DEBUG = 1 ]] ; then echo >&2 excess length without jpg $excess_length; fi
             if [[ excess_length_without_jpg -ge 0 ]] ; then
                 number_of_characters_to_keep=$(( number_of_characters_to_keep-excess_length_without_jpg ))
             fi
-            echo >&2 number of characters to keep after shortening: $number_of_characters_to_keep
+            if [[ $DEBUG = 1 ]] ; then echo >&2 number of characters to keep after shortening: $number_of_characters_to_keep; fi
             if [[ number_of_characters_to_keep -ge 0 ]] ; then
                 shortened_suffix=${suffix_for_json_files::number_of_characters_to_keep}
-                echo >&2 number of characters to keep $number_of_characters_to_keep
-                echo >&2 shortened suffix: \"$shortened_suffix\"
+                if [[ $DEBUG = 1 ]] ; then echo >&2 number of characters to keep $number_of_characters_to_keep; fi
+                if [[ $DEBUG = 1 ]] ; then echo >&2 shortened suffix: \"$shortened_suffix\"; fi
                 echo "$photo_file_without_jpg"$shortened_suffix.json
             else
                 echo >&2 cannot create metadata filename for: \"$photo_file\"
@@ -83,8 +83,8 @@ elif [[ $total_length -gt $MAX_LENGTH ]] ; then
         fi
     elif [[ number_of_characters_to_keep -ge 0 ]] ; then
         shortened_suffix=${suffix_for_json_files::number_of_characters_to_keep}
-        echo >&2 number of characters to keep $number_of_characters_to_keep
-        echo >&2 shortened suffix: \"$shortened_suffix\"
+        if [[ $DEBUG = 1 ]] ; then echo >&2 number of characters to keep $number_of_characters_to_keep; fi
+        if [[ $DEBUG = 1 ]] ; then echo >&2 shortened suffix: \"$shortened_suffix\"; fi
         echo "$photo_file"$shortened_suffix.json
     fi
 else
